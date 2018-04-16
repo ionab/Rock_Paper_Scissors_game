3bf6c8ba77fbdd78f29f8cba08c7ec92432b0730
Kelsie Braidwood <kelsieiona@gmail.com>
2018-04-16 17:41:37 +0100

    extensions added, with CSS

--- app.rb
+++ app.rb
@@ -3,6 +3,10 @@ require "sinatra/contrib/all" if development?
 also_reload("./models/*")
 require_relative "./models/game.rb"
 
+get "/" do 
+  erb(:home)
+end
+
 get "/game/:player1/:player2" do
   @winner = Game.play(params[:player1], params[:player2])
   erb(:result)
